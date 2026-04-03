af472e3cb2f4f0848967beab8c08eff7a3e34532
unknown7751 <aryanjawanjal8@gmail.com>
2026-04-04 01:09:00 +0530

    Updated smart contract

--- backend/src/routes/__init__.py
+++ backend/src/routes/__init__.py
@@ -1,5 +1,6 @@
 from .accidents import router as accidents_router
 from .volunteers import router as volunteers_router
 from .tasks import router as tasks_router
+from .rewards import router as rewards_router
 
-__all__ = ["accidents_router", "volunteers_router", "tasks_router"]
+__all__ = ["accidents_router", "volunteers_router", "tasks_router", "rewards_router"]
